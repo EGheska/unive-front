261c14f0336d6990ece5cfbb89da73fa05cf49f3
EGheska <dimanich300@gmail.com>
2024-12-20 13:43:18 +0100

    finilized the navbar

--- src/components/header.js
+++ src/components/header.js
@@ -10,7 +10,6 @@ const Header = () => {
             <a href="/sign" className="logout" style={{textDecoration:"none"}}>
                 <img src="./exit-svgrepo-com 1.svg" alt="logout"></img> Log out
             </a>
-
         </div>
     )
 }
