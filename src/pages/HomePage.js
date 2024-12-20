908309aa7843da4e113757ecfa96fe7c14cd9abe
EGheska <dimanich300@gmail.com>
2024-12-20 13:57:29 +0100

    added a personal information dto render

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -3,6 +3,17 @@ import '../styles/homepage.css';
 import Navbar from "../components/navbar";
 import Header from "../components/header";
 
+
+const personalInfoDTO = {
+    firstName: "Dima",
+    infix: "—",
+    surname: "Taras",
+    dateOfBirth: "11.04.2002",
+    address: "Haverstraatpassage 12B , 7511EW , ENSCHEDE",
+    email: "unknown@who.knows.email",
+    phoneNumber: "+12 3 45 67 89 00",
+};
+
 const HomePage = () => {
     return (
         <div className="pageClass-home">
@@ -14,28 +25,74 @@ const HomePage = () => {
             <div className="main-container">
                 <Navbar className="nav"/>
                 <div className="content-container">
-                    <div className="personal-information-container">
-
+                    <div className="personal-info-container">
+                        <h2>Personal Information</h2>
+                        <table className="personal-info-table">
+                            <tbody>
+                            {personalInfoDTO.firstName && (
+                                <tr>
+                                    <td>First name</td>
+                                    <td>{personalInfoDTO.firstName}</td>
+                                </tr>
+                            )}
+                            {personalInfoDTO.infix && (
+                                <tr>
+                                    <td>Infix</td>
+                                    <td>{personalInfoDTO.infix}</td>
+                                </tr>
+                            )}
+                            {personalInfoDTO.surname && (
+                                <tr>
+                                    <td>Surname</td>
+                                    <td>{personalInfoDTO.surname}</td>
+                                </tr>
+                            )}
+                            {personalInfoDTO.dateOfBirth && (
+                                <tr>
+                                    <td>Date of birth</td>
+                                    <td>{personalInfoDTO.dateOfBirth}</td>
+                                </tr>
+                            )}
+                            {personalInfoDTO.address && (
+                                <tr>
+                                    <td>Address</td>
+                                    <td>{personalInfoDTO.address}</td>
+                                </tr>
+                            )}
+                            {personalInfoDTO.email && (
+                                <tr>
+                                    <td>Email</td>
+                                    <td>{personalInfoDTO.email}</td>
+                                </tr>
+                            )}
+                            {personalInfoDTO.phoneNumber && (
+                                <tr>
+                                    <td>Telephone number</td>
+                                    <td>{personalInfoDTO.phoneNumber}</td>
+                                </tr>
+                            )}
+                            </tbody>
+                        </table>
                     </div>
-                    <div className="notifications-container">
+                        <div className="notifications-container">
 
-                    </div>
-                    <div className="policies-container">
+                        </div>
+                        <div className="policies-container">
 
-                    </div>
-                    <div className="claims-container">
+                        </div>
+                        <div className="claims-container">
 
-                    </div>
-                    <div className="payment-container">
+                        </div>
+                        <div className="payment-container">
 
-                    </div>
-                    <div className="faq-container">
+                        </div>
+                        <div className="faq-container">
 
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-    )
-}
+            )
+            }
 
-export default HomePage;
+            export default HomePage;
