175b7ec2125e6f5c78ce4a98bf9bc46ee44d07c9
EGheska <dimanich300@gmail.com>
2024-12-23 14:29:13 +0100

    added icons to the navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -8,24 +8,59 @@ const Navbar = () => {
     return (
         <div className="navbar-container">
             <div className={`navbar-nav ${location.pathname === "/" ? "selected" : ""}`}>
-                <NavLink to="/" className="navbar-link">Dashboard</NavLink>
+                {location.pathname === "/" ? (
+                <img src="/dashboard_selected.png" alt="Dashboard" className="icon-unselected"/>
+            ) : (
+                <img src="/dashboard_icon.png" alt="Dashboard" className="icon-unselected"/>
+            )}
+                <NavLink to="/" className="navbar-link">
+                    Dashboard
+                </NavLink>
             </div>
             <div className={`navbar-nav ${location.pathname === "/personal-info" ? "selected" : ""}`}>
-                <NavLink to="/personal-info" className="navbar-link">Personal Info.</NavLink>
+                {location.pathname === "/personal-info" ? (
+                    <img src="/personal_info_selected.svg" alt="Dashboard" className="icon-unselected"/>
+                ) : (
+                    <img src="/personal_info_icon.svg" alt="Dashboard" className="icon-unselected"/>
+                )}
+                <NavLink to="/personal-info" className="navbar-link">
+                    Personal Info.
+                </NavLink>
             </div>
             <div className={`navbar-nav ${location.pathname === "/notifications" ? "selected" : ""}`}>
+                {location.pathname === "/notifications" ? (
+                    <img src="/notifications_icon_selected.png" alt="Dashboard" className="icon-unselected"/>
+                ) : (
+                    <img src="/notifications_unselected.png" alt="Dashboard" className="icon-unselected"/>
+                )}
                 <NavLink to="/notifications" className="navbar-link">Notifications</NavLink>
             </div>
             <div className={`navbar-nav ${location.pathname === "/policies" ? "selected" : ""}`}>
+                {location.pathname === "/policies" ? (
+                    <img src="/policy_saelected.png" alt="Dashboard" className="icon-unselected"/>
+                ) : (
+                    <img src="/policies_icon.png" alt="Dashboard" className="icon-unselected"/>
+                )}
                 <NavLink to="/policies" className="navbar-link">Policies</NavLink>
             </div>
             <div className={`navbar-nav ${location.pathname === "/claims" ? "selected" : ""}`}>
+                {location.pathname === "/claims" ? (
+                    <img src="/claims_selected.png" alt="Dashboard" className="icon-unselected"/>
+                ) : (
+                    <img src="/claims_icon.png" alt="Dashboard" className="icon-unselected"/>
+                )}
                 <NavLink to="/claims" className="navbar-link">Claims</NavLink>
             </div>
             <div className={`navbar-nav ${location.pathname === "/payments" ? "selected" : ""}`}>
+                {location.pathname === "/payments" ? (
+                    <img src="/payments_selected.png" alt="Dashboard" className="icon-unselected"/>
+                ) : (
+                    <img src="/payments_icon.png" alt="Dashboard" className="icon-unselected"/>
+                )}
                 <NavLink to="/payments" className="navbar-link">Payments</NavLink>
             </div>
             <div className={`navbar-nav ${location.pathname === "/faq" ? "selected" : ""}`}>
+                {/*<img src="/dashboard_icon.png" alt="Dashboard" className="icon-unselected"/>*/}
                 <NavLink to="/faq" className="navbar-link">FAQ</NavLink>
             </div>
         </div>
