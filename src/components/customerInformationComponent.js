8dfdd24bb6ebbfd87a5c44b2c082f73093717233
EGheska <dimanich300@gmail.com>
2025-01-08 18:58:19 +0100

    finished customer information component layout

---
+++ src/components/customerInformationComponent.js
@@ -0,0 +1,30 @@
+import React from "react";
+import "../styles/customerInformationComponent.css"
+
+
+const CustomerInformationComponent = ({personalInfoDTO}) => {
+    return (
+        <div className="popup-information">
+            <img src="a" alt={"Client"} className="customer-image"/>
+            <table className="customer-info-table">
+                <thead>
+                <tr>
+                    <th>Customer Information</th>
+                </tr>
+                </thead>
+                <tbody>
+                {Object.entries(personalInfoDTO).map(([key, value]) => (
+                    <tr key={key}>
+                        <td>
+                            {key.replace(/([a-z])([A-Z])/g, '$1 $2')}
+                        </td>
+                        <td className="field-value">{value}</td>
+                    </tr>
+                ))}
+                </tbody>
+            </table>
+        </div>
+    )
+}
+
+export default CustomerInformationComponent;
--- src/pages/employeeCustomerLibrary.js
+++ src/pages/employeeCustomerLibrary.js
@@ -3,6 +3,20 @@ import Navbar from "../components/navbar";
 import Header from "../components/header";
 import CustomerSearchComponent from "../components/customerSearchComponent";
 import "../styles/employeeCustomerLibrary.css"
+import CustomerInformationComponent from "../components/customerInformationComponent";
+
+
+const personalInfoDTO = {
+    CustomerNo: "CUST110293847",
+    firstName: "Dima",
+    infix: "—",
+    surname: "Taras",
+    dateOfBirth: "11.04.2002",
+    address: "Haverstraatpassage 12B , 7511EW , ENSCHEDE",
+    email: "unknown@who.knows.email",
+    phoneNumber: "+12 3 45 67 89 00",
+}
+
 
 const EmployeeCustomerLibrary = () => {
     const [isCustomerSelected, setIsCustomerSelected] = useState(false);
@@ -23,13 +37,8 @@ const EmployeeCustomerLibrary = () => {
                     <CustomerSearchComponent onCustomerSelected={handleCustomerSelection}/>
                     {isCustomerSelected && (
                         <div className="popup-container">
-                            <div className="popup-information">
-                                <img src="a" alt={"Client"}/>
-                                <table>
-
-                                </table>
-                            </div>
-                            <button>
+                           <CustomerInformationComponent personalInfoDTO={personalInfoDTO}/>
+                            <button className="edit-button-cl">
                                 Edit <span></span>
                             </button>
                             <div className="popup-detailed-information">
